39921ef65b1fcd86cdd1e88582b3da9c39b4e7f6
Grace Tang <83622471+developer-grace@users.noreply.github.com>
2021-08-04 21:06:49 +1000

    Week 1 HelloWorld task

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Message myMessage;
+            myMessage = new Message("Hello World...");
+            myMessage.Print();
+
+            Console.WriteLine("Enter name:");
+            string name;
+            name = Console.ReadLine ();
+
+            if (name == "Grace")
+            {
+                myMessage = new Message("Welcome back, Grace! Ready to girlboss, gaslight, gatekeep today?");
+                myMessage.Print();
+            }
+
+            else if (name == "Richard")
+            {
+                myMessage = new Message("Hello Dick. Don't forget to pay your reparations to the land you've stolen from.");
+                myMessage.Print();
+            }
+
+            else if (name == "Ella")
+            {
+                myMessage = new Message("Welcome back, Ella!");
+                myMessage.Print();
+            }
+
+            else
+            {
+                myMessage = new Message("That's a bit of a silly name innit luv?");
+                myMessage.Print();
+            }
+        }
+    }
+}
