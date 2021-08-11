b089c911f5dbfc64501784323531160a8bc1b1fe
Grace Tang <grace.tang1028@gmail.com>
2021-08-11 16:46:58 +1000

    Adding week 2 stuff

---
+++ Week-2/ShapeDrawer/Program.cs
@@ -0,0 +1,20 @@
+using System;
+using SplashKitSDK;
+
+namespace ShapeDrawer
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            new Window("Shape Drawer", 800, 600);
+            do
+            {
+                SplashKit.ProcessEvents();
+                SplashKit.ClearScreen();
+
+                SplashKit.RefreshScreen();
+            } while (!SplashKit.WindowCloseRequested("Shape Drawer"));
+        }
+    }
+}
---
+++ Week-2/ShapeDrawer/Shape.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using SplashKitSDK;
+namespace ShapeDrawer
+{
+    public class Shape
+    {
+        private float _x, _y;
+        private int _width, _height;
+        private Color _color;
+
+        public Shape()
+        {
+            _color = Color.Green;
+            _x = 0; _y = 0;
+            _width = 100; _height = 100;
+        }
+
+        public Color Color
+        {
+            get
+            {
+                return _color;
+            }
+            set
+            {
+               _color = value;
+            }
+
+        }
+
+        public float X
+        {
+            get
+            {
+                return _x;
+            }
+            set
+            {
+                _x = value;
+            }
+        }
+
+        public float Y
+        {
+            get
+            {
+                return _y;
+            }
+            set
+            {
+                _y = value;
+            }
+        }
+
+        public int Width
+        {
+            get
+            {
+                return _width;
+            }
+            set {
+                _width = value; /* value is an int datatype keyword, ask about the keyword here for a float; do we need a cast? */
+            }
+        }
+
+        public int Height
+        {
+            get
+            {
+                return _height;
+            }
+            set
+            {
+                _height = value;
+            }
+        }
+
+        public void Draw()
+        {
+            SplashKit.FillRectangle(_color, _x, _y, _width, _height);
+        }
+        public bool IsAt(Point2D pt)
+        {
+            if ((_x > 0) && (_y > 0))
+            {
+                return true;
+            }
+            //if ((_x < 100) && (_y < 100))
+            //{
+            //    return true;
+            //}
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
