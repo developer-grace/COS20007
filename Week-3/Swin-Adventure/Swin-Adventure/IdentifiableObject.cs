e52219663b4d5dfbefbe04f777e036c73bed9767
Grace Tang <grace.tang1028@gmail.com>
2021-08-16 13:00:57 +1000

    Deleted previous Swin-Adventure files in Week-2; Initial commit for Swin-Adventure: Iteration 1 in Week-3

---
+++ Week-3/Swin-Adventure/Swin-Adventure/IdentifiableObject.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Swin_Adventure
+{
+    public class IdentifiableObject
+    {
+        private List<string> _identifiers;
+
+        public IdentifiableObject(string[] idents)
+        {
+            foreach(string id in idents)
+            {
+                this.AddIdentifier(id);
+            }
+        }
+
+        public Boolean AreYou(string id)
+        {
+            if(_identifiers.Contains(id))
+            {
+                return true;
+            }
+            return false;
+        }
+        
+        public void AddIdentifier( string id )
+        {
+            _identifiers.Add(id.ToLower());
+        }
+    }
+}
