fdc9ed8e736558a399e8f10c798fbfb5cf3e0f39
Grace Tang <grace.tang1028@gmail.com>
2021-08-16 15:29:38 +1000

    Week 3 part 6

--- Week-3/ShapeDrawer/Drawing.cs
+++ Week-3/ShapeDrawer/Drawing.cs
@@ -1,10 +1,36 @@
 ﻿using System;
+using SplashKitSDK;
+using System.Collections.Generic;
+
 namespace ShapeDrawer
 {
     public class Drawing
     {
-        public Drawing()
+        private readonly List<Shape> _shapes;
+        private Color color;
+        private Color _background;
+
+
+        public Drawing(Color background)
+        {
+            _shapes = new List<Shape>();
+        }
+
+        public Drawing() : this(Color.White)
         {
         }
+
+        public Color Background
+        {
+            get
+            {
+                return _background;
+            }
+            set
+            {
+                _background = value;
+            }
+        }
+
     }
 }
