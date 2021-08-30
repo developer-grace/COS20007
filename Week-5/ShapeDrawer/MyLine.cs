5b9c93c5f5d21b5028eda28eb776ac579a1eb1d7
Grace Tang <grace.tang1028@gmail.com>
2021-08-30 17:53:54 +1000

    4.1P Created MyLine class, implemented features as child class of Shape

---
+++ Week-5/ShapeDrawer/MyLine.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using SplashKitSDK;
+namespace ShapeDrawer
+{
+    public class MyLine : Shape 
+    {
+        private int _length;
+        public MyLine(Color clr) : base (clr)
+        {
+            _length = 150;
+        }
+
+        public MyLine() : this(Color.Black)
+        {
+
+        }
+
+        public override void Draw()
+        {
+            if (Selected)
+                DrawOutline();
+            
+        }
+
+        public override void DrawOutline()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool IsAt(Point2D pt)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Length
+        {
+            get { return _length; }
+            set { _length = value; }
+        }
+    }
+}
