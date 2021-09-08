24aa71029a84bded37698ccd5062b23b1e5917d2
Grace Tang <grace.tang1028@gmail.com>
2021-09-08 12:50:05 +1000

    4.2P Created new unit test for Item class

---
+++ Week-5/SwinAdventure/SwinAdventure/SwinAdventureTest/ItemTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using SwinAdventure;
+namespace SwinAdventureTest
+{
+    [TestFixture]
+    public class ItemTests
+    {
+        private Item _testableItem;
+        private string[] _testStrings = new string[] { "Test string 1", "Test Item 2", "Test Item 3" };
+
+
+
+        [SetUp]
+        public void SetUp()
+        {
+            _testableItem = new Item(_testStrings, "Test Item", "This is a test item");
+        }
+
+        [Test]
+        public void TestIdentifiable()
+        {
+            Assert.IsTrue(_testableItem.AreYou("Test string 1"));
+            Assert.IsTrue(_testableItem.AreYou("Test string 2"));
+            Assert.IsTrue(_testableItem.AreYou("Test string 3"));
+            Assert.IsFalse(_testableItem.AreYou("Test string 4"));
+
+        }
+    }
+}
+
