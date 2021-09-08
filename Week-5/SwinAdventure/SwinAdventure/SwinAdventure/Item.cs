27b6c36d3740bbcdc424719b01228e6b3d870dfa
Grace Tang <grace.tang1028@gmail.com>
2021-09-08 15:54:12 +1000

    4.2P Changed line 13 to call ToString from base object, and removed unnecessary fields in Item object

--- Week-5/SwinAdventure/SwinAdventure/SwinAdventure/Item.cs
+++ Week-5/SwinAdventure/SwinAdventure/SwinAdventure/Item.cs
@@ -3,18 +3,14 @@ namespace SwinAdventure
 {
     public class Item : GameObject
     {
-        private string _name;
-        private string _desc;
 
         public Item(string[] idents, string name, string desc) : base(idents, name, desc)
         {
-            _name = name;
-            _desc = desc;
         }
 
         public override string ToString()
         {
-            return _name.ToString();
+            return base.ToString();
         }
     }
 }
