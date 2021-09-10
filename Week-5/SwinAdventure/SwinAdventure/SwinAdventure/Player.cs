a09be19ca7328604a9c15022573a6f0d781b42d8
Grace Tang <grace.tang1028@gmail.com>
2021-09-10 16:30:13 +1000

    4.2P WIP Adding a Locate method to Player class

--- Week-5/SwinAdventure/SwinAdventure/SwinAdventure/Player.cs
+++ Week-5/SwinAdventure/SwinAdventure/SwinAdventure/Player.cs
@@ -9,9 +9,24 @@ namespace SwinAdventure
         {
         }
 
+        public GameObject Locate(string id)
+        {
+            // checking if "id" is in the Player's Inventory
+            if (_inventory.HasItem(id))
+            {
+                // if yes, ask if an Item is "id" and if it matches the 
+                // identifier we are looking for then fetch it and return it
+
+                return _inventory.Fetch(id);
+            }
+
+            // otherwise return nothing
+            return null;
+        }
+
         public override string FullDescription
         {
-            get { return FullDescription; }
+            get { return "You are carrying: " + ShortDescription; }
         }
 
         public Inventory Inventory
