0b01f84080c3a2e7ebd8d67698d131396a0df779
Grace Tang <grace.tang1028@gmail.com>
2021-09-23 16:35:14 +1000

    5.1P Created unit test for Bag class

---
+++ Week-7/SwinAdventure - 5.1P/SwinAdventure/SwinAdventureTest/BagTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using SwinAdventure;
+namespace SwinAdventureTest
+{
+    [TestFixture]
+    public class BagTest
+    {
+        // Add private fields here
+
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        [Test]
+        public void TestBagLocateItems()
+        {
+
+        }
+
+        [Test]
+        public void TestBagLocateItself()
+        {
+
+        }
+
+        [Test]
+        public void TestBagLocateNothing()
+        {
+
+        }
+
+        [Test]
+        public void TestBagFullDesc()
+        {
+
+        }
+
+        [Test]
+        public void TestBagInBag()
+        {
+
+        }
+    }
+}
