91519f0d40bf395d7af2b9f883cd525213b26a3c
Grace Tang <grace.tang1028@gmail.com>
2021-09-23 16:29:35 +1000

    5.1P Created Bag class

---
+++ Week-7/SwinAdventure - 5.1P/SwinAdventure/SwinAdventure/Bag.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+namespace SwinAdventure
+{
+    public class Bag : Item
+    {
+        private Inventory _inventory;
+        public Bag(string[] ids, string name, string desc) : base(ids, name, desc)
+        {
+            _inventory = new Inventory();
+        }
+
+        public GameObject Locate(string id)
+        {
+            if (AreYou(id)) // Are we trying to locate the Bag itself?
+            {
+                return this;
+            }
+            // If not, check if what we're looking for is in the Bag
+            else if (_inventory.HasItem(id))
+            {
+                // If yes, ask if an Item is "id" and if it matches the 
+                // identifier we are looking for then return it
+                return this;
+            }
+            else
+            {
+                // otherwise return nothing
+                return null;
+            }
+        }
+
+        public override string FullDescription
+        {
+            get
+            {
+                return "In the Bag you can see: " + ShortDescription;
+            }
+        }
+
+        public Inventory Inventory
+        {
+            get
+            {
+                return Inventory;
+            }
+        }
+    }
+}
