6d455be16376d3f514f60c7b0b1166e3483d0fd1
Grace Tang <grace.tang1028@gmail.com>
2021-09-27 18:33:25 +1000

    Task 1: Added code to main program - WIP

--- Mid-Semester-Test/Task1/LittleLibrary/LittleLibrary/Program.cs
+++ Mid-Semester-Test/Task1/LittleLibrary/LittleLibrary/Program.cs
@@ -6,7 +6,22 @@ namespace LittleLibrary
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Little Library!");
+
+            Console.WriteLine("Initialising Little Library...");
+            Library library = new Library("My Little Library");
+
+            Console.WriteLine("Initialising books and games...");
+            Book book1 = new Book("Natalie Tan’s Book of Luck and Fortune", "Roselle Lim", "1984803255");
+            Book book2 = new Book("The People We Keep", "Allison Larkin", "1982171294");
+            Game game1 = new Game("The Legend of Zelda: Breath of the Wild", "Nintendo EPD", "E+10");
+            Game game2 = new Game("Doki Doki Literature Club!", "Team Salvato", "PEGI 16");
+
+            Console.WriteLine("Adding a book to Library");
+            library.AddResource(book2);
+            Console.WriteLine("Adding a game to Library");
+            library.AddResource(game1);
+
         }
     }
 }
