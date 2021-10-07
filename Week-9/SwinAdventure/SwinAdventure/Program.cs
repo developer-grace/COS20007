5573ca649b9862e5e1e7f49d311b1e7073dbb4fd
Grace Tang <grace.tang1028@gmail.com>
2021-10-07 15:57:43 +1100

    6.1C Wrote main program - initialising player info, items, bag and inventory

--- Week-9/SwinAdventure/SwinAdventure/Program.cs
+++ Week-9/SwinAdventure/SwinAdventure/Program.cs
@@ -7,6 +7,29 @@ namespace SwinAdventure
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome, new player.");
+            Console.WriteLine("Please enter your name: ");
+            string playerName = Console.ReadLine();
+            Console.WriteLine("Tell us a bit about yourself!");
+            string playerDesc = Console.ReadLine();
+
+            Player player = new Player(playerName, playerDesc);
+
+            Item gem = new Item(new string[] { "gem", "ruby" }, "gem", "A bright red gem");
+            Item stick = new Item(new string[] { "stick" }, "stick", "A wooden stick");
+            player.Inventory.Put(gem);
+            player.Inventory.Put(stick);
+
+            Bag playerBag = new Bag(new string[] { "bag", "player's bag", "knapsack" }, "your bag", "A bag");
+            player.Inventory.Put(playerBag);
+
+            Item glass = new Item(new string[] { "magnifying glass", "magnify", "glass", "zoom in" }, "magnifying glass", "A magnifying glass for adventurers");
+            playerBag.Inventory.Put(glass);
+
+            string playerCommand = Console.ReadLine();
+            LookCommand look = new LookCommand(new string[] { "player" });
+            look.Execute(player, playerCommand.Split());
+
         }
     }
 }
