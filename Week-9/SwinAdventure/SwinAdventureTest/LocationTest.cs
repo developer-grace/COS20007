14c1e321ddab6da02b5d048980f8300d7eec77a6
Grace Tang <grace.tang1028@gmail.com>
2021-11-01 19:02:05 +1100

    6.1C Made the unit tests work

--- Week-9/SwinAdventure/SwinAdventureTest/LocationTest.cs
+++ Week-9/SwinAdventure/SwinAdventureTest/LocationTest.cs
@@ -21,8 +21,49 @@ namespace SwinAdventureTest
         [SetUp]
         public void SetUp()
         {
+            // Initialising stuff
+            _testLocation = new Location(new string[] { "here", "house" }, "Hero's House", "House where the hero resides");
+
+            _testPlayer = new Player("Link", "The hero", _testLocation);
+
+            _testGem = new Item(new string[] { "gem" }, "gem", "a bright red gem");
+            _testSword = new Item(new string[] { "sword" }, "sword", "a metal sword with sheath");
+
+
+
+            // Putting items into a location
+            _testLocation.PutItem(_testGem);
+            _testLocation.PutItem(_testSword);
 
         }
 
+        [Test]
+        // Test whether Location can identify themselves
+        public void TestAreYou()
+        {
+            Assert.IsTrue(_testLocation.AreYou("here"));
+        }
+
+        [Test]
+        // Test whether Locations can locate items they have
+        public void TestLocateItem()
+        {
+            Assert.AreEqual(_testSword, _testLocation.FetchItem("sword"));
+        }
+
+
+        [Test]
+        // Test whether Players can locate items in their location
+        public void TestPlayerLocation()
+        {
+            Assert.AreEqual(_testLocation.ToString(), _testPlayer.WhereAreYou());
+        }
+
+        [Test]
+        // Test whether Players can locate items in their location
+        public void TestPlayerLocateItem()
+        {
+            Assert.AreEqual(_testGem, _testPlayer.Locate("gem"));
+        }
     }
 }
