611401e391e356a3b8d6242fc5e1d7898543041b
Grace Tang <grace.tang1028@gmail.com>
2021-11-07 18:57:30 +1100

    10.1P Modified clock based on UML diagram in 3.2P

--- SWOTVAC/Clock_in_Javascript/clock.js
+++ SWOTVAC/Clock_in_Javascript/clock.js
@@ -1,21 +1,82 @@
-console.log('Loading clock class');
+console.log('Loading clock class, et al');
+
+class Counter {
+    constructor(name, overflow) {
+        this.name = name || "Default name";
+        this.ticks = 0;
+        this.overflow = overflow
+    }
+
+    // Returns true if we overflowed, false if not.
+    increment() {
+        this.ticks++;
+        if(this.ticks === this.overflow) {
+            this.ticks = 0;
+            return true;
+        } else {
+            return false;
+        }
+    }
+    reset() {
+        this.ticks = 0;
+    }
+
+    // Left pad the ticks with zeros.
+    toString() {
+        return `0${this.ticks}`.slice(-2);
+    }
+
+    set(value) {
+        if(value <= this.overflow) {
+            this.ticks = value;
+        } else {
+            throw new Error(`Tried to set counter to a value (${value}) greater than or equal to the overflow (${this.overflow})`);
+        }
+    }
+}
 
 class Clock {
 
-    constructor() {
-        this.currentTime = new Date(); // Note that declaring it this way makes it public and not protected
+    constructor(name = "Default name") {
+
+        this.name = name;
+
+        this.hours = new Counter('hours', 24);
+        this.minutes = new Counter('minutes', 60);
+        this.seconds = new Counter('seconds', 60);
+        
+
         this.printTime();
     }
 
     tick() {
-        this.currentTime.setSeconds(this.currentTime.getSeconds() + 1);
+        if(this.seconds.increment()) {
+            if(this.minutes.increment()) {
+                if(this.hours.increment()) {
+                    this.hours.reset();
+                }
+            }
+        }
         this.printTime();
     }
 
-    printTime() {
-        console.log(this.currentTime.toTimeString());
+    /**
+     * Give us the time in the format: "hh:mm:ss"
+     */
+    setTime(time) {
+        var parts = time.split(":").map(part => parseInt(part));
+        this.hours.set(parts[0]);
+        this.minutes.set(parts[1]);
+        this.seconds.set(parts[2]);
     }
 
+    printTime() {
+        console.log(this.toString());
+    }
+    toString() {
+        return `${this.hours}:${this.minutes}:${this.seconds}`;
+    }
+    
     resetTime() {
         this.currentTime = new Date('1-1-2021')
         this.printTime();
